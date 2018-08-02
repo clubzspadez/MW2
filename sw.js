83a9c5b4aa41c4caf66a731cbf3ab1429b8d49a1
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-08-01 20:51:07 -0400

    working on fetch req, res for service worker

--- sw.js
+++ sw.js
@@ -5,15 +5,15 @@
 const cache_version = 2;
 const cache_name = `restaurant-v${cache_version}`;
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/restaurant.html",
-  "/dist/css/",
-  "/dist/css/styles.css",
-  "/img/",
-  "/dist/js/",
-  "/dist/js/index.min.js",
-  "/dist/js/restaurant.min.js"
+  "./",
+  "./index.html",
+  "./restaurant.html",
+  "./dist/css/",
+  "./dist/css/styles.css",
+  "./img/",
+  "./dist/js/",
+  "./dist/js/index.min.js",
+  "./dist/js/restaurant.min.js"
 ];
 
 /**
@@ -21,7 +21,7 @@ const urlsToCache = [
  *
  * * pass cache_name, and use urlsToCache
  */
-self.addEventListener("install", event => {
+self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(cache_name).then(cache => {
       return cache.addAll(urlsToCache);
@@ -29,6 +29,23 @@ self.addEventListener("install", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
-  console.log(event.request.url);
+self.addEventListener("fetch", function(event) {
+  // responde to fetch event
+  const url = new URL(event.request.url).host;
+  // console.log(event.request.url);
+  // const pathName = url.pathname("/restaurants");
+  if (url === "localhost:1337") console.log("This is the servers host");
+
+  // with the current fetch event respond with
+  // response
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        console.log(response);
+        return response;
+      }
+
+      return fetch(event.request);
+    })
+  );
 });
